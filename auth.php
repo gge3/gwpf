addf284b28400c4b4e28047f5a475bcf2c5c3c98
gge3 <gil.grau.45@gmail.com>
2026-04-20 11:11:24 +0000

    authpassupdate

--- auth.php
+++ auth.php
@@ -7,7 +7,7 @@ define('ADMIN_PASS',     '1234');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'portfolio');
 define('DB_USER', 'portfolio_user');
-define('DB_PASS', 'password_forta_123');
+define('DB_PASS', 'PasswordSegur123!');
 
 // ── SESSION ────────────────────────────────────────────────────
 if (session_status() === PHP_SESSION_NONE) {
